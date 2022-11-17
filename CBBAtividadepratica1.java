fd6b122d1ffabd099a0df80f61a9c890e44c2fec
Débora Fernandes C <95572299+Deboraaluna@users.noreply.github.com>
2022-11-17 20:04:19 -0300

    Update CBBAtividadepratica1.java

--- CBBAtividadepratica1.java
+++ CBBAtividadepratica1.java
@@ -1,8 +1,10 @@
-//package banco;
-public abstract class Conta {
-    private int numero;
-    private String nome_Titular;
-    protected double saldo;
+public class CBBAtividadePratica1 {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        for (int y = 0; y < 100; y++) {
+            array[y] = y;
+            System.out.println(array[y]);
+        }
 
-  public abstract boolean sacar(double valor);
-  }
+    }
+}
